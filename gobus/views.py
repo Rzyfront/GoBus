70225ee482cbf8837bb9f27c314224032dca4f9d
Rafael Eduardo Martinez Frontado <103870044+Rzyfront@users.noreply.github.com>
2024-11-11 21:35:02 -0500

    rutas basicas y documentacion de api

--- gobus/views.py
+++ gobus/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .models import Agencia, Terminal, Ruta, Bus, Conductor, Pasajero, Boleto
+from .serializers import (
+    UserSerializer, AgenciaSerializer, TerminalSerializer, RutaSerializer,
+    BusSerializer, ConductorSerializer, PasajeroSerializer, BoletoSerializer
+)
+
+
+# ViewSet para el modelo de Usuario
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class AgenciaViewSet(viewsets.ModelViewSet):
+    queryset = Agencia.objects.all()
+    serializer_class = AgenciaSerializer
+
+
+class TerminalViewSet(viewsets.ModelViewSet):
+    queryset = Terminal.objects.all()
+    serializer_class = TerminalSerializer
+
+
+class RutaViewSet(viewsets.ModelViewSet):
+    queryset = Ruta.objects.all()
+    serializer_class = RutaSerializer
+
+
+class BusViewSet(viewsets.ModelViewSet):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+
+class ConductorViewSet(viewsets.ModelViewSet):
+    queryset = Conductor.objects.all()
+    serializer_class = ConductorSerializer
+
+
+class PasajeroViewSet(viewsets.ModelViewSet):
+    queryset = Pasajero.objects.all()
+    serializer_class = PasajeroSerializer
+
+
+class BoletoViewSet(viewsets.ModelViewSet):
+    queryset = Boleto.objects.all()
+    serializer_class = BoletoSerializer
